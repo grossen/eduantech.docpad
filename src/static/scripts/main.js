da54545d1ebc0da45957d363ff687330781b139d
Greduan <eduanlavaque@gmail.com>
2013-10-05 16:58:17 -0500

    Special sticky menu
    Thanks to:
    http://andrewhenderson.me/tutorial/jquery-sticky-sidebar/

---
+++ src/static/scripts/main.js
@@ -0,0 +1,17 @@
+// Activate CSS emoticonization
+$('#wrapper').emoticonize();
+
+// http://andrewhenderson.me/tutorial/jquery-sticky-sidebar/
+$(function(){ // document ready
+	var stickyTop = $('#header nav').offset().top; // returns number
+	$(window).scroll(function(){ // scroll event
+		var windowTop = $(window).scrollTop(); // returns number
+		if (stickyTop < windowTop) {
+			$('#header nav').css({position:'fixed', top:0, background:'#fff', width:'100%'});
+			$('#content').css('margin-top','70px');
+		} else {
+			$('#header nav').css({position:'static'});
+			$('#content').css('margin-top','0');
+		}
+	});
+});
